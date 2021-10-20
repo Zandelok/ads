c45002d63a8c72e0d427269689cfa1323a8091f6
Kirill Gleb <gleb.kirill98@gmail.com>
2021-10-20 16:15:46 +0300

    Made some fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@
 #
 #  id          :bigint           not null, primary key
 #  description :string
-#  email       :string
-#  name        :string
-#  surname     :string
+#  email       :string           not null
+#  name        :string           not null
+#  surname     :string           not null
 #
 class User < ApplicationRecord
-  validates_presence_of :name, :surname
+  validates_presence_of :name, :surname, :email
   validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 2021_10_19_130934) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "surname"
-    t.string "email"
+    t.string "name", null: false
+    t.string "surname", null: false
+    t.string "email", null: false
     t.string "description"
   end
 
